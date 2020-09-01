e8a35670dd84cc456c138e8df181860a60ddabc5
Ben Ropple <36946060+bropple@users.noreply.github.com>
2020-09-01 14:50:42 -0400

    Add files via upload
    Updated camera photo settings. Added a 1s delay, hoping the sensor adjusts in that time frame. Settings had no effect if picture is taken immediately after.

--- PIRcamera.ino
+++ PIRcamera.ino
@@ -42,7 +42,7 @@ int pictureMultiplier = 1;
  */
   
 void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable the brownout detector
+  //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable the brownout detector
   Serial.begin(115200); //set baud rate
   Serial.setDebugOutput(true); //ESP will print current state at boot
 
@@ -55,56 +55,9 @@ void setup() {
 //    }
 //  EEPROM.end();
 
-  //camera configuration from Espressif example code
- 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  
   pinMode(4, INPUT); //remove the camera flash RTC hold while the program runs
   digitalWrite(4, LOW);
   rtc_gpio_hold_dis(GPIO_NUM_4);
- 
-  if(psramFound()){ //there will be PSRAM, but just in case
-    config.frame_size = FRAMESIZE_UXGA; //The largest frame size, 1600x1200
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
- 
-  // Initialize the camera
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-
-    sensor_t * s = esp_camera_sensor_get();
-    //changing settings here to try and fix contrast issues
-    s->set_brightness(s, -2);     // -2 to 2
-    s->set_contrast(s, 1);       // -2 to 2
-    s->set_saturation(s, -1);     // -2 to 2
  
   Serial.println("Initializing SD card...");
 
@@ -117,7 +70,9 @@ void setup() {
     Serial.println("No SD card inserted!");
     return;
   }
-   
+
+  camera_setup();
+  
   camera_fb_t * fb = NULL;
  
   fb = esp_camera_fb_get();  //Take a picture
@@ -167,4 +122,88 @@ void setup() {
  
 void loop() { //not used
  
+}
+
+bool camera_setup(){
+  //camera configuration from Espressif example code
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if(psramFound()){ //there will be PSRAM, but just in case
+    config.frame_size = FRAMESIZE_UXGA; //The largest frame size, 1600x1200
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize the camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return 0;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+
+  if  (s == NULL) {
+    Serial.println("Problem setting camera parameters");
+    return 0;
+  }
+  
+  //changing settings here to try and fix contrast issues
+    //s->set_framesize(s, FRAMESIZE_UXGA);
+    //s->set_quality(s, 10);
+    s->set_brightness(s, -2);
+    s->set_contrast(s, 1);
+    s->set_saturation(s, 1);
+
+    s->set_aec_value(s, 0);
+    s->set_aec2(s, 1);
+    s->set_ae_level(s, 0);
+
+    s->set_whitebal(s, 1);
+    s->set_awb_gain(s, 1);
+    s->set_wb_mode(s, 0);
+    
+    s->set_agc_gain(s, 1);
+    s->set_gainceiling(s, (gainceiling_t)0);
+    
+    s->set_gain_ctrl(s, 1);
+    s->set_exposure_ctrl(s, 1);
+    s->set_hmirror(s, 0);
+    s->set_vflip(s, 0);
+    
+    s->set_dcw(s, 1);
+    s->set_bpc(s, 0);
+    s->set_wpc(s, 1);
+    s->set_raw_gma(s, 1);
+    s->set_lenc(s, 1);
+    s->set_special_effect(s, 0);
+    s->set_colorbar(s, 0);
+
+    delay(1000); //hopefully gives the camera time to adjust
+
+  return 1;
 }
